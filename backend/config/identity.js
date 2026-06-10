83daefc9c86617a3e8b53a4925157b3bc1271aac
Yash Pandey <yp15royal@gmail.com>
2026-06-10 19:52:42 +0530

    Update identity config with Yash's credentials

--- backend/config/identity.js
+++ backend/config/identity.js
@@ -1,5 +1,5 @@
 module.exports = {
-  user_id: "john_doe_17091999",
-  email_id: "john.doe@sit.edu",
-  enrollment_number: "SIT20260610"
+  user_id: "yash_pandey_15022004",
+  email_id: "yp15royal@gmail.com",
+  enrollment_number: "23070122234"
 };
